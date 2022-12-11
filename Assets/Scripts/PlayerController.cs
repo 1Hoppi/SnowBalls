892b76fe9e3f7b192d6f9fc41db8cc45b852b696
LeSaR-desktop <ilkov2006@gmail.com>
2022-12-11 12:04:15 +0300

    added animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
 	
+	public Animator animator;
 	private Rigidbody2D rb;
 
 	public float speed = 10f;
 	public float jumpForce = 5f;
-	public bool isGrounded = true;
+	public bool isGrounded = false,
+		isReloading = false;
 
-	private Vector2 oldVelocity = Vector2.zero;
-
+	private int collisionsCount = 0;
 
 
 
 	public KeyCode
 		upKey = KeyCode.W,
 		rightKey = KeyCode.D,
-		leftKey = KeyCode.A;
+		leftKey = KeyCode.A,
+		reloadKey = KeyCode.R;
+
+
 
 	public void Start(){
 
@@ -28,29 +32,58 @@ public class PlayerController : MonoBehaviour{
 	}
 
 	public void Update(){
+		
+		if(Input.GetKeyDown(reloadKey) && isGrounded)
+			StartCoroutine(Reload());
 
-		float input = (Input.GetKey(rightKey) ? 1 : 0) - (Input.GetKey(leftKey) ? 1 : 0);
-		rb.velocity = new Vector2(input * speed, rb.velocity.y);
+		if(!isReloading){
+
+			float input = (Input.GetKey(rightKey) ? 1 : 0) - (Input.GetKey(leftKey) ? 1 : 0);
+			rb.velocity = new Vector2(input * speed, rb.velocity.y);
 
 
 
-		CheckGrounded();
+			if(Input.GetKeyDown(upKey) && isGrounded){
 
-		if(Input.GetKeyDown(upKey) && isGrounded){
+				rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+				isGrounded = false;
 
-			rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-			isGrounded = false;
+			}
 
 		}
+
+
+
+		animator.SetBool("Jumping", !isGrounded);
+		animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
 		
 	}
 
-	private void CheckGrounded(){
+	private void OnCollisionEnter2D(Collision2D collision){
 
-		if(rb.velocity.y == 0 && oldVelocity.y != 0) isGrounded = true;
-		else if(oldVelocity.y == 0 && rb.velocity.y != 0) isGrounded = false;
+		if(collision.gameObject.CompareTag("Environment")) collisionsCount++;
+        if(collisionsCount > 0) isGrounded = true;
 
-		oldVelocity = rb.velocity;
+    }
+    private void OnCollisionExit2D(Collision2D collision){
+
+        if(collision.gameObject.CompareTag("Environment")) collisionsCount--;
+        if(collisionsCount == 0) isGrounded = false;
+
+    }
+
+	private IEnumerator Reload(){
+		
+		isReloading = true;
+		rb.velocity = Vector2.zero;
+
+		animator.SetBool("Reloading", true);
+		animator.SetTrigger("Reload");
+
+		yield return new WaitForSeconds(1f);
+		isReloading = false;
+		
+		animator.SetBool("Reloading", false);
 
 	}
 
--- Assets/Scripts/ThrowController.cs
+++ Assets/Scripts/ThrowController.cs
@@ -3,52 +3,105 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ThrowController : MonoBehaviour{
+	
+	public GameObject Sprite;
+	private Animator animator;
+	private new SpriteRenderer renderer;
 
+	public Transform player1, player2;
+	public GameObject snowballPrefab;
+	
 	public KeyCode shootKey = KeyCode.S;
 
-	public GameObject snowballPrefab;
+	public bool canThrow = true;
+	public float throwCooldown = 1f;
 
 	public float snowballSpeed = 30f;
 
 	public float rot = 0f;
 	public float minRot = -30f, maxRot = 60f;
-	public float rotSpeed = 20f;
+	public float rotSpeed = 100f;
 
 
+
+	private void Start(){
+		
+		animator = Sprite.GetComponent<Animator>();
+		renderer = Sprite.GetComponent<SpriteRenderer>();
+
+	}
 
 	void Update(){
 
-		rot += rotSpeed * Time.deltaTime;
-		if(rot > maxRot){
+		float realRot, realMinRot, realMaxRot, realRotSpeed;
+		if(player1.position.x <= player2.position.x){
 
-			rot = maxRot;
-			rotSpeed = -rotSpeed;
+			realRot = rot;
+			realMinRot = minRot;
+			realMaxRot = maxRot;
+			realRotSpeed = rotSpeed;
+			renderer.flipX = false;
 
-		}else if(rot < minRot){
+		}else{
 
-			rot = minRot;
-			rotSpeed = -rotSpeed;
-
-		}
-
-		transform.rotation = Quaternion.Euler(0, 0, rot);
-
-
-
-		if(Input.GetKeyDown(shootKey)){
-
-			GameObject snowball = Instantiate(snowballPrefab);
-
-			snowball.transform.position = transform.position + transform.rotation * Vector2.right * 0.75f;
-			snowball.GetComponent<Rigidbody2D>().velocity =
-				transform.rotation * Vector2.right * snowballSpeed;
-
-			Destroy(snowball, 5f);
-
-			
+			realRot = 180f - rot;
+			realMinRot = 180f - maxRot;
+			realMaxRot = 180f - minRot;
+			realRotSpeed = -rotSpeed;
+			renderer.flipX = true;
 
 		}
 
+		realRot += realRotSpeed * Time.deltaTime;
+		if(realRot > realMaxRot){
+
+			realRot = realMaxRot;
+			rotSpeed *= -1;
+
+		}else if(realRot < realMinRot){
+
+			realRot = realMinRot;
+			rotSpeed *= -1;
+
+		}
+
+		if(player1.position.x <= player2.position.x)
+			rot = realRot;
+		else
+			rot = 180f - realRot;
+
+		transform.rotation = Quaternion.Euler(0, 0, realRot);
+
+
+
+		if(Input.GetKeyDown(shootKey))
+			StartCoroutine(Throw());
+
+	}
+
+	private IEnumerator Throw(){
+
+		if(!canThrow) yield break;
+
+		canThrow = false;
+
+		animator.SetBool("Throwing", true);
+
+		yield return new WaitForSeconds(0.25f);
+
+		GameObject snowball = Instantiate(snowballPrefab);
+		Destroy(snowball, 5f);
+
+		snowball.transform.position = transform.position + transform.rotation * Vector2.right * 0.75f;
+		snowball.GetComponent<Rigidbody2D>().velocity =
+			transform.rotation * Vector2.right * snowballSpeed;
+
+		animator.SetBool("Throwing", false);
+
+		yield return new WaitForSeconds(throwCooldown);
+
+		canThrow = true;
+		
 	}
 
 }
